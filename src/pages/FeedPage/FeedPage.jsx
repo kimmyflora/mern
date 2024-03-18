69e1a453d1820a2c7fbd651d7eeb8d5e8cfd48fc
User Name <kimmyflora270@gmail.com>
2024-03-18 14:05:39 -0700

    ok

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,18 +1,29 @@
 import Header from "../../components/Header/Header";
 import { Grid } from "semantic-ui-react";
-import AddFormPost from "../AddPostForm/AddPostForm";
 
 import tokenService from '../../utils/tokenService';
+import { Route, Routes } from "react-router-dom";
+import RestaurantPage from "../RestaurantPage"
+
 
 export default function FeedPage(){
 
 
 
    return (
-    <h1>main page
-       <AddFormPost/>  
+
+
+      <Routes>
+         <Route path="/food" element={<Food/>} />
+
+    
+    <h1>Click on the categories you wish to view more of!
+      
      
     </h1>
+
+
+  </Routes>
    
   )
 }
---
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -0,0 +1,6 @@
+
+export default function RestaurantPage(){
+    return(
+        <h1>List of all Small Business owned Restaurants</h1>
+    )
+}
